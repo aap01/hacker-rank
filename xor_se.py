9094a4dd4df6a3caf6ad2fa965788ec814a31bd8
aap01 <aap.pavel@gmail.com>
2022-04-19 02:30:41 +0600

    tle on xor-se problem

---
+++ xor_se.py
@@ -0,0 +1,99 @@
+#https://www.hackerrank.com/challenges/xor-se/problem?isFullScreen=true
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the xorSequence function below.
+
+
+def xorSequence(l, r):
+    # In a=[0, 1, 3, 0, 4, 1, 7, 0, 8, 1, 11, 0, 12, 1, 15, 0...]
+    # a[i * 4 - 1] = 0
+    # indexOfZero = i * 4 - 1
+    # indexOfZero = (l // 4) * 4 - 1 [i = l // 4]
+    # So, a[indexOfZero + 1] = indexOfZero + 1
+
+    # TLE
+    # indexOfZero = (l // 4) * 4 - 1
+    # if indexOfZero < 0:
+    #     indexOfZero = 0
+    # print(indexOfZero)
+    # al = 0
+    # for i in range(indexOfZero + 1, l + 1):
+    #     al = al ^ i
+    # a = [al]
+    # k = 0
+    # for i in range(l + 1, r + 1):
+    #     a.append(a[k] ^ i)
+    #     k += 1
+    # res = 0
+    # for i in a:
+    #     res ^= i
+    # return res
+
+    # as Ax = Ax-1 ^ x
+    # Al ^ Al+1
+    # = Al ^ (Al ^ l+2)
+    # = l+2
+
+    # Al ^ Al+1 ^ Al+2
+    # = Al ^ Al+1 ^ (Al+1 ^ l+2)
+    # = Al ^ l+2
+
+    # Al ^ Al+1 ^ Al+2 ^ Al+3 = Al ^ Al+1 ^ Al+2 ^ ((Al+2)^l+3)
+    # = Al ^ Al+1 ^ (l+3) = Al ^ (Al ^ l+1) ^ L+3
+    # = (l+1) ^ (l+3)
+
+    # Al ^ Al+1 ^ Al+2 ^ Al+3 ^ Al+4
+    # = Al ^ Al+1 ^ Al+2 ^ Al+3 ^ ((Al+3)^l+4)
+    # = Al ^ Al+1 ^ Al+2 ^ (l+4)
+    # = Al ^ Al+1 ^ (Al+1 ^ l+2) ^ L+4
+    # = Al ^ l+2 ^ L+4
+
+    # So if there are even number of terms in l->r
+    # res = l+1 ^ l+3 ^ .. r
+    # else
+    # res = Al ^ l+2 ^ l+4
+
+    indexOfZero = (l // 4) * 4 - 1
+    if indexOfZero < 0:
+        indexOfZero = 0
+    al = 0
+    res = 0
+    if (r - l + 1) % 2 == 0:
+        k = l + 1
+        while k <= r:
+            res ^= k
+            k += 2
+    else:
+        for i in range(indexOfZero + 1, l + 1):
+            al = al ^ i
+        res = al
+        k = l + 2
+        while k <= r:
+            res ^= k
+            k += 2
+    return res
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        lr = input().split()
+
+        l = int(lr[0])
+
+        r = int(lr[1])
+
+        result = xorSequence(l, r)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
