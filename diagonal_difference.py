d961db6edb21122e97201d6ef2362bda9dbd4eed
aap01 <aap.pavel@gmail.com>
2022-04-26 04:21:55 +0600

    solved diagonal-difference problem

---
+++ diagonal_difference.py
@@ -0,0 +1,44 @@
+#https://www.hackerrank.com/challenges/one-month-preparation-kit-diagonal-difference/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+
+def diagonalDifference(arr):
+    # Write your code here
+    diff = 0
+    for i in range(0, len(arr)):
+        l = len(arr[i])
+        d1 = arr[i][i]
+        d2 = arr[l - 1 - i][i]
+        diff += d1 - d2
+
+    return diff if diff >= 0 else -diff
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
