7e96fd60ffb4a5f9aace0af3f9e52b77c89d3528
aap01 <aap.pavel@gmail.com>
2022-04-26 00:52:33 +0600

    solved sparse-arrays problem

---
+++ sparse_arrays.py
@@ -0,0 +1,54 @@
+#https://www.hackerrank.com/challenges/one-month-preparation-kit-sparse-arrays/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+
+def matchingStrings(strings, queries):
+    # Write your code here
+    m = dict()
+    for item in strings:
+        if item not in m:
+            m[item] = 0
+        m[item] += 1
+    return [m[i] if i in m else 0 for i in queries]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
