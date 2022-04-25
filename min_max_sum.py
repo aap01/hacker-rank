d3a1a9086cf8d9c1c90a4ea3def82ed8ebe9bd36
aap01 <aap.pavel@gmail.com>
2022-04-26 00:32:42 +0600

    solved min-max-sum problem

---
+++ min_max_sum.py
@@ -0,0 +1,29 @@
+#https://www.hackerrank.com/challenges/one-month-preparation-kit-mini-max-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one&h_r=next-challenge&h_v=zen
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+
+def miniMaxSum(arr):
+    # Write your code here
+    arr = sorted(arr)
+    mn = sum(arr[:4])
+    mx = sum(arr[1:])
+    print(mn, mx, sep=' ')
+
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
