1d1c989a5fd33510486eb496856f73b3414b9c39
aap01 <aap.pavel@gmail.com>
2022-04-26 00:46:31 +0600

    solved time-conversion problem

---
+++ time_conversion.py
@@ -0,0 +1,41 @@
+#https://www.hackerrank.com/challenges/one-month-preparation-kit-time-conversion/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+
+def timeConversion(s):
+    # Write your code here
+    am = 'AM'
+    pm = 'PM'
+    i = s[:2]
+    i = int(i) % 12
+    if s[-2:] == pm:
+        i += 12
+    i = str(i)
+    if len(i) == 1:
+        i = "0" + i
+    return i + s[2: -2]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
