a93eddb337d106041343bb864257bfd70814a838
aap01 <aap.pavel@gmail.com>
2022-04-26 23:56:38 +0600

    solved pangrams problem

---
+++ pangrams.py
@@ -0,0 +1,40 @@
+#https://www.hackerrank.com/challenges/one-month-preparation-kit-pangrams/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one&h_r=next-challenge&h_v=zen
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+
+def pangrams(s):
+    # Write your code here
+    m = dict()
+    count = 0
+    for i in s:
+        if i != " " and i.lower() not in m:
+            m[i.lower()] = True
+            count += 1
+        if count >= 26:
+            return 'pangram'
+    return 'not pangram'
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
