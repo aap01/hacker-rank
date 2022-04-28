4b85eebcf813fc47a7ed2092f3029fa5e40b6e44
aap01 <aap.pavel@gmail.com>
2022-04-28 19:23:26 +0600

    solved sock-merchant problem

---
+++ sock_merchant.py
@@ -0,0 +1,44 @@
+#https://www.hackerrank.com/challenges/one-month-preparation-kit-sock-merchant/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-two
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+
+def sockMerchant(n, ar):
+    # Write your code here
+    m = dict()
+    pair = 0
+    for i in ar:
+        if i not in m:
+            m[i] = 0
+        m[i] += 1
+        if m[i] % 2 == 0:
+            pair += 1
+    return pair
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
