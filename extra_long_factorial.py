5278dfe6ee1985bf05fcfd4eca3af6fcd6b1d3a7
aap01 <aap.pavel@gmail.com>
2022-05-13 13:47:01 +0600

    solved extra-long-factorials problem

--- extra_long_factorial.py
+++ extra_long_factorial.py
@@ -15,37 +15,18 @@ import sys
 
 
 def extraLongFactorials(n):
-    # Write your code here
-    def add(p, q, c, bit):
-        if len(q) > len(p):
-            return add(q, p, c, bit)
-        if len(p) >= bit:
-            lenQ = len(q)
-            cN, s = add(p[: -lenQ], q, c, bit)
-
-            if cN != "0":
-                return "0", cN + s
-            return "0", s
-        if c == "0":
-            s = str(int(p) + int(q))
-            print(s)
-            if len(s) > len(q):
-                return s[0], s[1:]
-            return "0", s
-        return add(p, str(int(q) + int(c)), "0", bit)
-
-    def mul(p, q, bit):
-        if len(q) > len(p):
-            return mul(q, p, bit)
-        if len(p) + len(q) >= bit:
-            lenP = len(p)
-            x = mul(p[: lenP//2], q, bit) + "0" * (lenP//2 + 1)
-            y = mul(p[lenP // 2:], q, bit)
-            return add(x, y, bit)
-        x = int(p)
-        y = int(q)
-        return str(x * y)
-    return add('10', '1', '1', 2)
+    res = [1]
+    c = 0
+    for i in range(2, n + 1):
+        for j in range(0, len(res)):
+            p = len(res) - j - 1
+            tmp = i * res[p] + c
+            res[p] = tmp % 10
+            c = tmp // 10
+        while c > 0:
+            res = [c % 10] + res
+            c = c // 10
+    print(''.join([str(i) for i in res]))
 
 
 if __name__ == '__main__':
